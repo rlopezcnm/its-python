c7a5103c963e2c966887d6bdf7ed73bf53b6bbe3
Robert Lopez <rlopez@cnm.edu>
2012-06-12 10:05:15 -0600

    Added exercises to ex8

---
+++ ex1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# File:       ex1.py
+# Author:     Robert Lopez
+#
+# Started on  Thu May 31 16:18:07 2012
+# Last-Updated Thu May 31 16:18:07 2012
+#
+# Description: Exercise 1 from LPTHW 2nd E
+#
+
+print "Hello Class!"
+print 'Hello Class!'
+print "This is enclosed in double quotation marks."
+print 'This is enclosed in single quotation marks.'
+print ("This is enclosed in double quotation marks within parenthesis.")
+print "Why should I 'like' typ'n this?"
+print "I'd much rather NOT type this."
+print '"Oh please", I \'said\', when the book "said" \"You can\'t touch this!\"'
+print "# To the left is an ocrothorpe character used to comment in Python."
+
+# # The output of this program was:
+# rlopez@rlopez ~/Programming/python/ITS python class $ ./ex1.py
+# Hello Class!
+# Hello Class!
+# This is enclosed in double quotation marks.
+# This is enclosed in single quotation marks.
+# This is enclosed in double quotation marks within parenthesis.
+# Why should I 'like' typ'n this?
+# I'd much rather NOT type this.
+# "Oh please", I 'said', when the book "said" "You can't touch this!"
+# # To the left is an ocrothorpe character used to comment in Python.
